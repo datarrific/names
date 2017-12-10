1956a5f5e91a609161cbc30649180490310e80e0
Jan Lelis <mail@janlelis.de>
2017-12-10 11:06:55 +0100

    Display Berlin districts

--- NameMap.js
+++ NameMap.js
@@ -14,8 +14,22 @@ export default class NameMap extends React.Component {
     mapboxgl.accessToken = 'pk.eyJ1IjoiZGF0YXJpZmZpYyIsImEiOiJjamF6ajVtNGMwbnFvMnduMjRib2p3ZDhsIn0.vCAR2WQrwxRPisWy7gTgpQ';
     var map = new mapboxgl.Map({
       container: 'name-map',
-      style: 'mapbox://styles/mapbox/light-v9'
+      style: 'mapbox://styles/mapbox/light-v9',
+      center: [13.2846504, 52.5069704],
+      zoom: 10,
     })
+
+    map.on("load", () => {
+      map.addLayer({
+        "id": "districts",
+        "source": {
+          type: "geojson",
+          data: "https://api.mapbox.com/datasets/v1/datariffic/cjazl0x06878o2xr1r0cznmxo/features?access_token=pk.eyJ1IjoiZGF0YXJpZmZpYyIsImEiOiJjamF6ajVtNGMwbnFvMnduMjRib2p3ZDhsIn0.vCAR2WQrwxRPisWy7gTgpQ",
+        },
+        "type": "line"
+      })
+    })
+
     this.setState({ map })
   }
 
